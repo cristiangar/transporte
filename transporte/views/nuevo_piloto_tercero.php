4e2ddb9be7b94c6daa7a60cd87a0528fdb71c83f
cristiangar <cristian_garc14@outlook.com>
2021-04-29 20:01:19 -0600

    Update nuevo_piloto_tercero.php

--- transporte/views/nuevo_piloto_tercero.php
+++ transporte/views/nuevo_piloto_tercero.php
@@ -32,215 +32,238 @@
 </nav>
 <div class="container-fluid">
     <form action="">
-    <h1>Datos del Piloto</h1>
-    <br>
-        <div class="form-row">
-            <div class="col-sm-4">
-            <label>Nombre</label>
-                <input type="text" class="form-control" placeholder="Nombre" require>
-            </div>
-            <div class="col-sm-4">
-            <label>Apellido</label>
-                <input type="text" class="form-control" placeholder="Apellido"require>
-            </div>
-            <div class="col-sm-4">
-            <label>Teléfono 1</label>
-                <input type="text" class="form-control" placeholder="Telefono 1" require>
-            </div>
-            <div class="col-sm-4">
-            <label>Teléfono 2</label>
-                <input type="text" class="form-control" placeholder="Teléfono 2" require>
-            </div>
-            <div class="col-sm-4">
-            <label>No.DPI</label>
-                <input type="text" class="form-control" placeholder="Número de DPI" require>
-            </div>
-            <div class="col-sm-4">
-            <label>Dirección</label>
-                <input type="text" class="form-control" placeholder="Dirección" require>
+        <h1>Datos del Piloto</h1>
+        <br>
+            <div class="form-row">
+                <div class="col-sm-4">
+                <label>Nombre</label>
+                    <input type="text" class="form-control" placeholder="Nombre" require>
+                </div>
+                <div class="col-sm-4">
+                <label>Apellido</label>
+                    <input type="text" class="form-control" placeholder="Apellido"require>
+                </div>
+                <div class="col-sm-4">
+                <label>Teléfono 1</label>
+                    <input type="text" class="form-control" placeholder="Telefono 1" require>
+                </div>
+                <div class="col-sm-4">
+                <label>Teléfono 2</label>
+                    <input type="text" class="form-control" placeholder="Teléfono 2" require>
+                </div>
+                <div class="col-sm-4">
+                <label>No.DPI</label>
+                    <input type="text" class="form-control" placeholder="Número de DPI" require>
+                </div>
+                <div class="col-sm-4">
+                <label>Dirección</label>
+                    <input type="text" class="form-control" placeholder="Dirección" require>
+                </div>
+
+                <div class="col-sm-4">
+                <br>
+                <label>Imagen de DPI</label>
+                <div class="container-fluid">
+                    <input type="file" name="imagen">
+                </div>
+                </div>
+                    
             </div>
 
-            <div class="col-sm-4">
             <br>
-            <label>Imagen de DPI</label>
-            <div class="container-fluid">
+            <h1>Datos de licencia </h1>
+            <br>
+            <div class="form-row">
+                <div class="col-sm-4">
+                    <label>No. Licencia</label>
+                    <input type="text" class="form-control" placeholder="Número Licencia" require>
+                </div>
+
+                <div class="col-sm-4">
+                    <label>Tipo Licencia</label>
+                    <select name="departamento" id="" class="form-control">
+                        <option value="1">A</option>
+                        <option value="2">B</option>
+                        <option value="3">C</option>
+                    </select>
+                </div>
+
+                <div class="col-sm-4">
+                    <label>Imagen Licencia</label>
+                    <div class="container-fluid">
+                        <input type="file" name="imagen">
+                    </div>
+                </div>
+                <br>
+            </div>
+
+            <br>
+            <h1>Datos de Pasaporte </h1>
+            <br>
+            <div class="form-row">
+                <div class="col-sm-4">
+                    <label>No. Pasaporte</label>
+                    <input type="text" class="form-control" placeholder="Número de Pasaporte" require>
+                </div>
+
+
+                <div class="col-sm-4">
+                    <label>Imagen Pasaporte</label>
+                    <div class="container-fluid">
+                        <input type="file" name="imagen">
+                    </div>
+                </div>
+                <br>
+            </div>
+
+            <br>
+            <h1>Datos del Vehiculo </h1>
+            <br>
+            <div  class="form-row">
+                <div class="col-sm-4">
+                    <label>No.placa</label>
+                    <input type="text" class="form-control" placeholder="Número de placa" require>
+                </div>
+
+
+                <div class="col-sm-4">
+                    <label>Marca del vehiculo</label>
+                    <div class="container-fluid">
+                    <input type="text" class="form-control" placeholder="Marca" require>
+                    </div>
+                </div>
+                <br>
+
+                <div class="col-sm-4">
+                <label>Tamaño</label>
+                    <input type="text" class="form-control" placeholder="Tamaño"require>
+                </div>
+
+                <div class="col-sm-4">
+                <label>Color</label>
+                    <input type="text" class="form-control" placeholder="Color" require>
+                </div>
+
+                <div class="col-sm-4">
+                <label>Peso</label>
+                    <input type="text" class="form-control" placeholder="Peso en Kilogramos" require>
+                </div>
+
+
+                <div class="col-sm-4">
+                <label>Ejes</label>
+                <input type="text" class="form-control" placeholder="Ejes" require>
+                </div>
+
+                <div class="col-sm-4">
+                <br>
+                <label>Imagen de Tarjeta de Cirtuclación</label>
                 <input type="file" name="imagen">
-            </div>
-            </div>
-                
-        </div>
+                </div>
 
-        <br>
-        <h1>Datos de licencia </h1>
-        <br>
-        <div class="form-row">
-            <div class="col-sm-4">
-                <label>No. Licencia</label>
-                <input type="text" class="form-control" placeholder="Número Licencia" require>
+
             </div>
 
-            <div class="col-sm-4">
-                <label>Tipo Licencia</label>
-                <select name="departamento" id="" class="form-control">
-                    <option value="1">A</option>
-                    <option value="2">B</option>
-                    <option value="3">C</option>
-                </select>
-            </div>
+            <br>
+            <h1>Datos del remolque</h1>
+            <br>
+            <br>
+            <div class="form-row">
+                    
+                    <div class="col-sm-4">
+                    <label>Marca</label>
+                        <input type="text" class="form-control" placeholder="Marca" require>
+                    </div>
 
-            <div class="col-sm-4">
-                <label>Imagen Licencia</label>
-                <div class="container-fluid">
+                    <div class="col-sm-4">
+                    <label>Tamaño</label>
+                        <input type="text" class="form-control" placeholder="Tamaño"require>
+                    </div>
+
+                    <div class="col-sm-4">
+                    <label>Color</label>
+                        <input type="text" class="form-control" placeholder="Color" require>
+                    </div>
+
+                    
+                    <div class="col-sm-4">
+                    <label>Placa</label>
+                        <input type="text" class="form-control" placeholder="Número de placa" require>
+                    </div>
+
+                    <div class="col-sm-4">
+                    <label>Peso</label>
+                        <input type="text" class="form-control" placeholder="Peso en Kilogramos" require>
+                    </div>
+
+
+                    <div class="col-sm-4">
+                    <label>Ejes</label>
+                    <input type="text" class="form-control" placeholder="Ejes" require>
+                    </div>
+
+                    <div class="col-sm-4">
+                    <label>Tipo de Remolque</label>
+                        <select class="form-control">
+                            <option>portaautomoviles</option>
+                            <option>plataforma abierta</option>
+                            <option>plataforma cerrada</option>
+                            <option>cisterna</option>
+                            <option>plataforma refigerada</option>
+                        </select>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <div class="col-sm-4">
+                    <br>
+                    <label>Imagen de Tarjeta de Cirtuclación</label>
                     <input type="file" name="imagen">
-                </div>
-            </div>
-            <br>
-        </div>
+                    </div>
+            
+                    <div class="col-sm-4">
+                    <label>Otros</label>
+                    <br>
+                    <textarea></textarea> 
+                    </div>
 
-        <br>
-        <h1>Datos de Pasaporte </h1>
-        <br>
-        <div class="form-row">
-            <div class="col-sm-4">
-                <label>No. Pasaporte</label>
-                <input type="text" class="form-control" placeholder="Número de Pasaporte" require>
-            </div>
-
-
-            <div class="col-sm-4">
-                <label>Imagen Pasaporte</label>
-                <div class="container-fluid">
-                    <input type="file" name="imagen">
-                </div>
-            </div>
-            <br>
-        </div>
-
-        <br>
-        <h1>Datos del Vehiculo </h1>
-        <br>
-        <div class="form-row">
-            <div class="col-sm-4">
-                <label>No.placa</label>
-                <input type="text" class="form-control" placeholder="Número de placa" require>
-            </div>
-
-
-            <div class="col-sm-4">
-                <label>Marca del vehiculo</label>
-                <div class="container-fluid">
-                <input type="text" class="form-control" placeholder="Marca" require>
-                </div>
-            </div>
-            <br>
-
-            <div class="col-sm-4">
-            <label>Tamaño</label>
-                <input type="text" class="form-control" placeholder="Tamaño"require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Color</label>
-                <input type="text" class="form-control" placeholder="Color" require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Peso</label>
-                <input type="text" class="form-control" placeholder="Peso en Kilogramos" require>
-            </div>
-
-
-            <div class="col-sm-4">
-            <label>Ejes</label>
-            <input type="text" class="form-control" placeholder="Ejes" require>
-            </div>
-
-            <div class="col-sm-4">
-            <br>
-            <label>Imagen de Tarjeta de Cirtuclación</label>
-            <input type="file" name="imagen">
-            </div>
-
-
-        </div>
-
-        <br>
-        <h1>Datos del remolque</h1>
-        <h3>ingrese estos datos unicamente si fuera necesario</h3>
-        <br>
-        <div class="form-row">
-            <div class="col-sm-4">
-            <label>Marca</label>
-                <input type="text" class="form-control" placeholder="Marca" require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Tamaño</label>
-                <input type="text" class="form-control" placeholder="Tamaño"require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Color</label>
-                <input type="text" class="form-control" placeholder="Color" require>
             </div>
 
             
-            <div class="col-sm-4">
-            <label>Placa</label>
-                <input type="text" class="form-control" placeholder="Número de placa" require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Peso</label>
-                <input type="text" class="form-control" placeholder="Peso en Kilogramos" require>
-            </div>
 
 
-            <div class="col-sm-4">
-            <label>Ejes</label>
-            <input type="text" class="form-control" placeholder="Ejes" require>
-            </div>
-
-            <div class="col-sm-4">
-            <label>Tipo de Remolque</label>
-            <select class="form-control">
-                <option>portaautomoviles</option>
-                <option>plataforma abierta</option>
-                <option>plataforma cerrada</option>
-                <option>cisterna</option>
-                <option>plataforma refigerada</option>
-            </select>
-            </div>
-            <br>
-            <br>
-
-            <div class="col-sm-4">
-            <br>
-            <label>Imagen de Tarjeta de Cirtuclación</label>
-            <input type="file" name="imagen">
-            </div>
-     
-            <div class="col-sm-4">
-            <label>Otros</label>
-            <br>
-            <textarea></textarea> 
-            </div>
-
-        </div>
+                <div class="container-fluid">
+                    <br>
+                    <br>
+                    <center>
+                        <input type="submit" class="btn btn-success" value="Aceptar">
+                        <a href="menu_nuevo_piloto.php"><button type="button" class="btn btn-warning" >Regresar</button></a>
+                        <input type="submit" class="btn btn-danger" value="cancelar">
+                        
+                    </center>
+                </div>
 
 
-            <div class="container-fluid">
-                <br>
-                <br>
-            <center>
-                <input type="submit" class="btn btn-success" value="Aceptar">
-                <a href="menu_nuevo_piloto.php"><button type="button" class="btn btn-warning" >Regresar</button></a>
-                <input type="submit" class="btn btn-danger" value="cancelar">
-                
-            </center>
-            </div>
     </form>
+
 </div>
-</div>
+<button class="btn btn-warning" onclick="myFunction()">Precione este boton si el vehiculo contiene un remolque</button>
+                <div id="myDIV" style="border:1px solid black;">
+                    Este elemento aparece y desaparece con el botón
+                </div>
+<script>
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+</script>
+
+
+
 </body>
 </html>
