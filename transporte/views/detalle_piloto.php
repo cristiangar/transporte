9967bb0fd65d569c6a63d010c5b75b66b37fb934
cristiangar <cristian_garc14@outlook.com>
2021-05-22 10:39:01 -0600

    Update detalle_piloto.php

--- transporte/views/detalle_piloto.php
+++ transporte/views/detalle_piloto.php
@@ -34,7 +34,9 @@
 </nav>
 <br>
 <br>
-<div class="container-fluid " style="width:20rem;margin:20px 0 24px 0">
+<div class="container-fluid">
+<center>
+<div class="container-fluid  bg-primary"style="width:50rem;margin:20px 0 24px 0" >
 <?php
 $id=$_GET['id'];
 include_once("../controller/pilotoInterno.php");
@@ -59,13 +61,47 @@ while ($row=mysqli_fetch_array($dt)) {
 
   }
 ?>
-<h1><?php echo $nombre." ".$apellido;?></h1>
-  <div class="card">
-      <div class="card-body">
-          <h2 class="card-title">DPI: <?php echo $dpi;?></h2>
-      </div>
-      <img src="<?php $ruta_imagen_dpi;?>" style="width:100%" alt="">
-  </div>
+<h1>Nombre del piloto: <?php echo $nombre." ".$apellido;?></h1>
+        <div class="card container-fluid">
+            <div class="card-body container-fluid">
+                <h2 class="card-title">DPI: <?php echo $dpi;?></h2>
+                <h2 class="card-title">Telefono: <?php echo $telefono1;?></h2>
+                <h2 class="card-title">Telefono 2: <?php echo $telefono2;?></h2>
+                <h2 class="card-title">Correo electronico: <?php echo $correo;?></h2>
+            </div>
+            <img src="<?php echo  $ruta_imagen_dpi;?>" style="width:100%" alt="">
+        </div>
+        <br>
+
+        <div class="card container-fluid">
+            <div class="card-body">
+                <h2 class="card-title">No. licencia: <?php echo $licencia;?></h2>
+                <h2 class="card-title">Tipo de licencia: <?php echo $tipo_licencia;?></h2>
+            </div>
+            <img src="<?php echo  $ruta_imagen_licencia;?>" style="width:100%" alt="">
+        </div>
+        <br>
+        <div class="card container-fluid">
+            <div class="card-body">
+                <h2 class="card-title">No. pasaporte: <?php echo $pasaporte;?></h2>
+            </div>
+            <img src="<?php echo  $ruta_imagen_pasaporte;?>" style="width:100%" alt="">
+        </div>
+        <br>
+        <div class="card container-fluid">
+            <div class="card-body">
+                <h2 class="card-title">No. caat: <?php echo $codigo_caat;?></h2>
+            </div>
+            <img class='img-fluid' onclick="javascript:this.width=450;this.height=338" ondblclick="javascript:this.width=100;this.height=80" src="<?php echo  $ruta_imagen_caat;?>" style="width:100%" alt="">
+        </div>
+        <br>
+</div>
+</center>
+
+<center>
+    <a href="menu_nuevo_piloto.php"><button type="button" class="btn btn-success" >Modificar</button></a>
+    <a href="choferes.php"><button type="button" class="btn btn-warning" >Regresar</button></a>       
+</center>
 </div>
 
 
