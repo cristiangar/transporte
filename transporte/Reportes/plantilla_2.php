84dfef9cfbb21b845b86f607dd3fb63a9ec61016
cristiangar <cristian_garc14@outlook.com>
2021-06-16 11:31:31 -0600

    reportes terminados

--- transporte/Reportes/plantilla_2.php
+++ transporte/Reportes/plantilla_2.php
@@ -8,11 +8,11 @@ class pdf extends fpdf
 	
 	function Header()
 	{
-		//$this->Image('',5,5,40);
+		$this->Image('../imagenes/logoE2.jpeg',10,5,30);
 		$this->SetFont('Arial','B',20);
 		$this->SetY(10);
 		$this->Cell(30);
-		$this->Cell(120,10,'Signacion de vehiclo y piloto',0,0,'C');
+		$this->Cell(120,10,'Asignacion de vehiclo y piloto',0,0,'C');
 		$this->Ln(20);
 	}
 
