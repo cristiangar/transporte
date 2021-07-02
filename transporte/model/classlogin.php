983f4fafbf5494cfa0572484ac9f6f8dfd894c8f
cristiangar <cristian_garc14@outlook.com>
2021-07-02 16:15:08 -0600

    login

--- transporte/model/classlogin.php
+++ transporte/model/classlogin.php
@@ -1,5 +1,6 @@
 <?php
 ob_start();
+session_start();
 include ('../Configuracion/config.php');
 class login
 {
@@ -13,15 +14,40 @@ class login
 
         $salida="SELECT @pn_usuario";
         $consultar=mysqli_query($db->objetoconexion,$salida);
+
+        $salida2="SELECT @pn_rol";
+        $consultar2=mysqli_query($db->objetoconexion,$salida2);
+
+        $salida3="SELECT @pn_id";
+        $consultar3=mysqli_query($db->objetoconexion,$salida3);
 		$db->desconectar();
+
         $res=mysqli_fetch_array($consultar);
         $texto=$res['@pn_usuario'];
+        $_SESSION['usuario']=$texto;
+        
+        $res2=mysqli_fetch_array($consultar2);
+        $texto2=$res2['@pn_rol'];
+        $_SESSION['rol']=$texto2;
+        
+
+        $res3=mysqli_fetch_array($consultar3);
+        $texto3=$res3['@pn_id'];
+        $_SESSION['idusuario']=$texto3;
+        
         if($texto=="ERROR")
         {
          header("Location: ../Index.php?error");
+         unset($_SESSION['usuario']);
+         unset($_SESSION['rol']);
+         unset($_SESSION['idusuario']);
+         
         }
         else{
-            echo "aceptado";
+            echo $_SESSION['usuario'];
+            echo $_SESSION['rol'];
+            echo $_SESSION['idusuario'];
+            header("Location: ../views/secritaria.php");
         }
 
 	}	
