938c7fd5367f6d156cc7deb160121e0b6521f689
cristiangar <cristian_garc14@outlook.com>
2021-07-06 11:45:13 -0600

    reportes

---
+++ transporte/Reportes/BusquedaReporte.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if(isset($_SESSION['usuario']))
+{
+    $rol=$_SESSION['rol'];
+    $us=$_SESSION['usuario'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nuevo Encabezado</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/imagen.css">
+    <script src="../js/jquery-3.3.1.min.js"></script>
+        <script src="../js/index.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <a href="../views/secritaria.php">
+        <img src="../imagenes/logo.png" alt="HTML tutorial" style="width:52px;height:52px;">
+    </a>
+    <ul class="navbar-nav ml-auto">
+        <li class="navbar-item">
+            <a class="nav-link" >Usuario: <?php echo $us;?></a>
+        </li>
+        <li class="navbar-item">
+            <a class="nav-link" href="../index.php">Cerrar sesión</a>
+        </li>
+    </ul>
+</nav>
+
+<div class="container-fluid">
+<br>
+<h1>Reportes</h1>
+</div>
+</div>
+<form action="">
+
+</form>
+</body>
+</html>
+<?php
+}
+
+else{/**else de la session */
+    header("location: ../Index.php");
+}/**ses */
+?>
--- transporte/views/secritaria.php
+++ transporte/views/secritaria.php
@@ -210,6 +210,17 @@ if(isset($_SESSION['usuario']))
                 </div>
             </div>
         </div>
+
+        <div class="wrapper fadeInDown col-sm-4"><!--efecto de caida-->
+            <div id="formContent"><!--contenedor-->
+                <div class="fadeIn first">
+                    <a href="../Reportes/BusquedaReporte.php">
+                    <img class="img-fluid" src="../imagenes/Reporte.jpg" id="icon" alt="User Icon" style="max-width:50%;width:auto;height:auto;">
+                    </a>
+                    <h1>Reportes</h1>
+                </div>
+            </div>
+        </div>
         
     </div>
 </div>
