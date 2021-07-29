d5d98b81a24a0b8a5f4d0f7ac061a76d0ec51e1b
jonatancotoc <jonatancotoc@gmail.com>
2021-07-28 18:05:19 -0600

    crud de vehiculos

--- transporte/views/secritaria.php
+++ transporte/views/secritaria.php
@@ -249,7 +249,7 @@ if(isset($_SESSION['usuario']))
         <div class="wrapper fadeInDown col-sm-4"><!--efecto de caida-->
             <div id="formContent"><!--contenedor-->
                 <div class="fadeIn first">
-                    <a href="../serviciovehiculos/listavehiculos.php?C">
+                    <a href="../serviciovehiculos/listavehiculos.php">
                     <img class="img-fluid" src="../imagenes/serviciocarro.jpg" id="icon" alt="User Icon" style="max-width:50%;width:auto;height:auto;">
                     </a>
                     <h1>Servicio de Vehiculos</h1>
